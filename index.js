b477d62f87c69559ba6031b23e4b008cfa0ebc87
biplob kumar <biplobkumar125@gmail.com>
2023-02-02 08:56:06 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+// Random  integir number 1-10 
+
+var integar=Math.round(Math.random() *10 );
+var number=prompt(" Type number 1-10:");
+
+if(number<=integar){
+  console.log("Great this number has been match ");
+}
+else{
+    console.log(" Not match this number ");
+}
+
+//  Random intigar number 
+
